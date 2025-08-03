29dc23bcbf799f648b7def93c79eb16ec449ceac
Prakash J <jprakash94@outlook.com>
2025-08-04 11:07:43 +1200

    feat(signup): add method to fill name on signup form
    Adds the `fillSignUpName` method to the Signup class. This method
    handles entering the user's name in the "New User Signup!" form.

--- pages/signup_page/signup.startSignUp.js
+++ pages/signup_page/signup.startSignUp.js
@@ -3,10 +3,15 @@ import { expect } from '@playwright/test';
 class Signup {
   constructor(page) {
     this.signUpPageConfirmation = page.getByRole('heading', { name: 'New User Signup!' });
+    this.name = page.getByPlaceholder('Name');
   }
 
   async verifySignupPageVisible() {
     await expect(this.signUpPageConfirmation).toBeVisible();
+  }
+
+  async fillSingUpName(name) {
+    await this.name.fille(name);
   }
 }
 
