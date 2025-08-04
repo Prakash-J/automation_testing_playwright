48fe9dab6a446cc96c8c41aa837510fde91f2c7b
Prakash J <jprakash94@outlook.com>
2025-08-04 12:31:25 +1200

    refactor(home-page): improve class structure and add documentation
    Refactors the Home page object for better clarity and alignment with project standards.
    
    - Renames the class from `Home` to `HomePage` for consistency with other page objects.
    - Adds JSDoc comments to the class and all public methods to improve code documentation and maintainability.

--- pages/home_page.js
+++ pages/home_page.js
@@ -3,24 +3,30 @@
  */
 class HomePage {
   /**
-   * @param {page} page
+   * @param {page} page The Playwright page object
    */
   constructor(page) {
     this.page = page;
+    /**
+     * The main logo of the website
+     */
     this.logo = page.locator('img[alt="Website for automation practice"]');
+    /**
+     * The main navigation bar list items
+     */
     this.navigationItems = page.locator('.navbar-nav li');
   }
 
   /**
-   * Navigate to home page
+   * Navigate to application's home page
    */
   async goToHomePage() {
     await this.page.goto('/');
   }
 
   /**
-   * Select the navigation bar link
-   * @param {linkName} linkName The navigation bar link name to be clicked
+   * Click specific link in the main navigation bar
+   * @param {string} linkName The visible text of the link to be clicked (eg. 'Signup')
    */
   async selectNavigationLink(linkName) {
     await this.page.getByRole('link', { name: linkName }).click();
