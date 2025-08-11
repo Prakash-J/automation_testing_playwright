56b282fecdec727a64de5d227cbe915cc4fc15c6
Prakash J <jprakash94@outlook.com>
2025-08-11 23:05:46 +1200

    feature(registerUser): End 2 End test case for user registration

--- src/tests/registerUser.spec.js
+++ src/tests/registerUser.spec.js
@@ -8,17 +8,41 @@ import { test, expect } from '../fixtures/registerUserFixture.ts';
  * Test suite for the user registration feature
  */
 test.describe('User Registration', () => {
-  test('Register User', async ({ homePage }) => {
+  test('Register User', async ({
+    userData,
+    homePage,
+    signUpPage,
+    accountInformationPage,
+    accountCreationConfirmationPage,
+    accountDeletionConfirmationPage,
+  }) => {
     // 1. Navigate to home page and verify it's loaded
     await homePage.goToHomePage();
     await expect(homePage.logo).toBeVisible();
 
-    // 3. Click signupPage button and verify signupPage page loaded
-    // await homePage.selectNavigationLink(' Signup / Login');
-    // await expect(signupPage.signUpPageTitle).toBeVisible();
+    // 2. Click signupPage button and verify signupPage page loaded
+    await homePage.selectNavigationLink(' Signup / Login');
+    await expect(signUpPage.signUpPageTitle).toBeVisible();
 
-    // // 4. Start the signupPage process with a unique email
-    // await signupPage.startSingup(user.name.full, user.contact.email);
-    // await expect(accountInfoPage.pageTitle).toBeVisible();
+    // 3. Start the signupPage process with a unique email
+    await signUpPage.startSignup(userData.userFullName, userData.email);
+    await expect(accountInformationPage.pageTitle).toBeVisible();
+
+    // 4. Fill the account information
+    await accountInformationPage.fillAccountDetails(userData);
+    const successfulAccountCreation = accountCreationConfirmationPage.accountCreationConfirmation;
+    await expect(successfulAccountCreation).toBeVisible();
+    await accountCreationConfirmationPage.clickContinueButton();
+
+    // 5. Validate successful login
+    const loggedInUserLocator = await homePage.getLoggedInUserLocator(userData.userFullName);
+    await expect(loggedInUserLocator).toBeVisible();
+
+    // 6. Delete the created account
+    await homePage.selectNavigationLink('Delete Account');
+    const deletedAccountConfirmation =
+      await accountDeletionConfirmationPage.deleteConfirmationMessage();
+    await expect(deletedAccountConfirmation).toBeVisible();
+    await accountDeletionConfirmationPage.clickContinue();
   });
 });
