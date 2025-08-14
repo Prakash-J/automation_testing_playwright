57e07855368b72c0c0f37c8adcb651909544fc85
Prakash J <jprakash94@outlook.com>
2025-08-14 16:32:32 +1200

    feature(existing user): Add new test case for login existing user and delete account
    - Test case will login existing user, navigate to homepage and click delete button
    - Configured the test case mode as default so that test case will run in sequence and there will be no overlap
    - Call userData directly in the spec file instead from fixture which help to avoid generate user data multiple time

---
+++ src/tests/userAuthentication.spec.js
@@ -0,0 +1,55 @@
+/**
+ * @file End-to-end tests for the user registration
+ */
+
+import userDataRaw from '../data/devData.ts';
+import { test, expect } from '../fixtures/authenticationFixture.ts';
+import testHelper from '../helper/testHelper.ts';
+
+const userData = userDataRaw;
+
+test.describe.configure({
+  mode: 'default',
+});
+
+/**
+ * Test suite for the user registration feature
+ */
+test.describe('User Authentication', () => {
+  test.describe.configure({ mode: 'default' });
+  test('SignUp and Fill Account Information', async ({
+    homePage,
+    signUpOrLoginPage,
+    accountInformationPage,
+    accountCreationConfirmationPage,
+  }) => {
+    await testHelper.navigateToHomeClickSignUpOrLoginButton(homePage, signUpOrLoginPage, 'signup');
+
+    await signUpOrLoginPage.startSignup(userData.userFullName, userData.email);
+    await expect(accountInformationPage.pageTitle).toBeVisible();
+
+    await accountInformationPage.fillAccountDetails(userData);
+    const successfulAccountCreation = accountCreationConfirmationPage.accountCreationConfirmation;
+    await expect(successfulAccountCreation).toBeVisible();
+    await accountCreationConfirmationPage.clickContinueButton();
+
+    await testHelper.checkUserNameInHomePage(homePage, userData.userFullName);
+  });
+
+  test('Login Existing User and Delete', async ({
+    homePage,
+    signUpOrLoginPage,
+    accountDeletionConfirmationPage,
+  }) => {
+    await testHelper.navigateToHomeClickSignUpOrLoginButton(homePage, signUpOrLoginPage, 'login');
+
+    await signUpOrLoginPage.startLogin(userData.email, userData.password);
+
+    await testHelper.checkUserNameInHomePage(homePage, userData.userFullName);
+    await homePage.selectNavigationLink('Delete Account');
+    const deletedAccountConfirmation =
+      await accountDeletionConfirmationPage.deleteConfirmationMessage();
+    await expect(deletedAccountConfirmation).toBeVisible();
+    await accountDeletionConfirmationPage.clickContinue();
+  });
+});
